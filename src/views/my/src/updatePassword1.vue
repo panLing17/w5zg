edc93abfb1f48e49dd1531c463688a0d6e785a5d
yuyi <yuyi@w5zg.com>
2018-04-10 20:23:50 +0800

    修改登录和支付密码

--- src/views/my/src/updatePassword1.vue
+++ src/views/my/src/updatePassword1.vue
@@ -5,114 +5,181 @@
         img(src="../../../assets/img/back@2x.png", style="width:.3rem", @click="$router.go(-1)")
       .topCenter(slot="center") {{titleName}}
     .form
-      w-input(v-model="form.mobile", label="手机号码：", label-width="2.5rem", placeholder="", input-button=true, :error="message", required) 
-      w-input(label="验证码：", label-width="2.5rem", placeholder="", input-button=true, :error="message", required, button-cover)
-        .inputButton(slot="button")
-      w-input(label="手机验证码：", label-width="2.5rem", placeholder="", :type="passwordType", input-button=true,v-model="form.password", required, button-cover)
-        .inputButton(slot="button").send 发送验证码
-      p.tips 系统将向您的手机号发送短信验证码  
-      button.regButton(@click="routergoupdata2()") 下一步                          
+      w-input(v-model="userData.mi_phone", label="手机号码：", label-width="2.5rem")
+      w-input(v-model="form.gCode",label="验证码：", label-width="2.5rem", placeholder="请输入验证码", input-button=true, :error="gCodeError", required, button-cover)
+        img.aplaceholder(slot="button", @click="getPicCode", :src="url")
+      w-input(v-model="form.vcode", label="手机验证码：", label-width="2.5rem", placeholder="请输入手机验证码", :error="checkCodeError", input-button=true, required, button-cover)
+        .inputButton(slot="button", @click="getPhoneCode", v-show="sendMsg" ,:class="{inputButtonGray:sendMsgStatus}") 获取验证码
+        .inputButton(slot="button", v-show="!sendMsg",style="background-color:gray") {{countDown}}
+      p.tips 系统将向您的手机号发送短信验证码
+      button.regButton(@click="nextStep",:class="{regButtonGray:nextStepStatus}") 下一步
 </template>
 <script>
- 
+  import {mapState} from 'vuex'
+
   export default {
     name: 'updatePassword1',
-    data () {
+    data() {
       return {
         form: {
-          mobile: '',
-          password: ''
+          gCode: '',
+          vcode: '',
+          type: '3'
         },
-        title:this.$route.query.routeParams,
-        titleName:''
+        version: 1,
+        url: this.$apiMember + 'member/picCode/150/75/60',
+        title: this.$route.query.routeParams,
+        sendMsgStatus: false,
+        nextStepStatus: true,
+        countDown: 60,
+        gCodeError: '',
+        checkCodeError: '',
+        sendMsg: true,
+        titleName: ''
       }
     },
-    created (){
+    computed: mapState(['userData']),
+    created() {
       if (this.title == 1) {
         this.titleName = "修改登录密码"
+        this.form.type = '3'
       }
       if (this.title == 2) {
         this.titleName = "修改支付密码"
+        this.form.type = '4'
       }
     },
-    mounted () {
- 
+    mounted() {
     },
     methods: {
-      routergoupdata2:function(){
-        console.log(this.title);
-        if (this.title == 1) {
-          this.$router.push({
-             name: '我的修改登录密码2',
-             query: {
-                routeParams: 1
-             }    
-          });
+      getPicCode() {
+        this.version += 1
+        this.url = this.$apiMember + 'member/picCode/150/75/60?v=' + this.version
+      },
+      getPhoneCode() {
+        // 发送验证码
+        let self = this
+        if (self.form.gCode == '') {
+          self.gCodeError = ''
+          return
         }
-        if (this.title == 2) {
-          this.$router.push({
-             name: '我的修改登录密码2',
-             query: {
-                routeParams: 2
-             }    
-          });
+
+        if (self.sendMsgStatus) {
+          return
         }
-        
+
+        self.$ajax({
+          method: 'post',
+          url: self.$apiMember + 'sms/sendCodeLoginIn',
+          params: self.form
+        }).then(function (response) {
+          if (response.data.optSuc) {
+            self.nextStepStatus = false
+            // 成功则开始读秒
+            self.sendMsg = false
+            let interval = window.setInterval(function () {
+              if ((self.countDown--) <= 0) {
+                self.countDown = 60
+                self.sendMsg = true
+                window.clearInterval(interval)
+              }
+            }, 1000)
+          } else {
+            self.getPicCode()
+          }
+        })
+      },
+      nextStep() {
+        let self = this
+        if (self.form.vcode == '' || self.nextStepStatus) {
+          self.checkCodeError = ''
+          return
+        }
+
+        self.$ajax({
+          method: 'post',
+          url: self.$apiMember + 'member/validationLoginIn',
+          params: self.form
+        }).then(function (response) {
+          if (response.data.optSuc) {
+            // 成功跳转页面
+            self.$router.push({name: '修改登录登录密码与支付密码验证码验证', query: {routeParams:self.title}})
+          }
+        })
       }
     }
   }
 </script>
 
 <style scoped>
-  .updatePassword1{
-  	height: 100vh;
-    background-color: rgb(242,242,242);
-  }
-  .updatePassword1 .topCenter{
-  	font-size: .5rem;
-  	font-weight: 400;
-  	color: rgb(51,51,51);
+  .updatePassword1 {
+    height: 100vh;
+    background-color: rgb(242, 242, 242);
   }
 
-  .form{
-    margin-top: 1.3rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  .updatePassword1 .topCenter {
+    font-size: .5rem;
+    font-weight: 400;
+    color: rgb(51, 51, 51);
   }
-  .inputButton{
-    width: 2rem;
-    height: .9rem;
-    background: #666;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .send{
+
+  .inputButton {
     width: 2rem;
     height: .9rem;
     border-radius: 1rem;
-    background: rgb(245,0,87);
+    background: rgb(245, 0, 87);
     color: white;
     display: flex;
     justify-content: center;
     align-items: center;
   }
+
+  .inputButtonGray {
+    background: rgb(192, 192, 192) !important;
+  }
+
+  .form {
+    margin-top: 1.3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .send {
+    width: 2rem;
+    height: .9rem;
+    border-radius: 1rem;
+    background: rgb(245, 0, 87);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   .regButton {
     margin-top: 1rem;
     width: 7rem;
     height: 1rem;
-    background-color: rgb(245,0,87);
+    background-color: rgb(245, 0, 87);
     color: white;
     font-size: .4rem;
     border: none;
     outline: none;
     border-radius: .5rem;
   }
-  .tips{
+
+  .regButtonGray {
+    background-color: rgb(192, 192, 192) !important;
+  }
+
+  .tips {
     font-size: .3rem;
-    color: rgb(153,153,153);
+    color: rgb(153, 153, 153);
     text-indent: 2rem;
   }
 
+  .aplaceholder {
+    width: 2rem;
+    background-color: rgb(245, 0, 87);
+  }
 </style>
