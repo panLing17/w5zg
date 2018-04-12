b72cc9d95f9789978bed0ddcdf44333e57d7507c
王恺强 <282294449@qq.com>
2018-04-12 17:04:19 +0800

    详情页修改，部分bug修复

---
+++ src/plugIn/alert/alert.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .alertBox
+    .bg
+    .main
+      p 一段介绍
+</template>
+
+<script>
+  export default {
+    name: 'alert',
+    props:{
+      value: String,
+      title: String
+    }
+  }
+</script>
+
+<style scoped>
+  .alertBox {
+
+  }
+  .alertBox .bg{
+    width: 100vh;
+    height: 100vh;
+    background-color: rgba(0,0,0,0.2);
+    position: fixed;
+  }
+</style>
