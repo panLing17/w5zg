cf0e0260d695bd2762d1184a51143227515575f2
王恺强 <282294449@qq.com>
2018-04-27 23:08:29 +0800

    详情页弹窗添加，alert修改

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
   name: 'app',
   mounted () {
     this.getDictionaries()
-    this.getLocation()
+    // this.getLocation()
     if (localStorage.hasOwnProperty('token')) {
       this.getUserData()
     }
