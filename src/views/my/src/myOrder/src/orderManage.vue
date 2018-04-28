cb1730b878597d9dd73f549cd569068d94f946e3
panling <panlingfn@126.com>
2018-04-28 16:57:44 +0800

    fix 订单管理end修复

--- src/views/my/src/myOrder/src/orderManage.vue
+++ src/views/my/src/myOrder/src/orderManage.vue
@@ -90,6 +90,11 @@
         this.mescroll.hideTopBtn();
         this.mescroll.destroy();
       },
+    watch: {
+        '$route' (to, from) {
+          this.mescroll.destroy();
+      }
+    },
       methods:{
         //回退判断
         backJump(){
