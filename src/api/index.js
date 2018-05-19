33a7a86cf4af80cac44682d708a133e3d8ae413e
panling <panlingfn@126.com>
2018-05-19 15:24:34 +0800

    update 请求失败放掉response

--- src/api/index.js
+++ src/api/index.js
@@ -26,6 +26,7 @@ axios.interceptors.response.use(
       return response
     } else {
       Message.error(res.msg)
+      return response
       // if (response.config.url.indexOf('member/login') === -1) {
       //   Message.error(res.msg)
       // }
