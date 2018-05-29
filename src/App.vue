692a5a8cb2cc6b793a4332a6a30fc73e47494021
王恺强 <282294449@qq.com>
2018-05-29 20:48:28 +0800

    解决选择省市滑动bug

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,6 @@
       // this.getLocation()
       if (localStorage.hasOwnProperty('token')) {
         this.getUserData()
-      } else {
-        this.$store.commit('setShowRegisterTicket', true)
       }
     },
     computed: {
@@ -126,19 +124,8 @@
           url: self.$apiMember + 'member/currentMember',
           params: {}
         }).then(function (response) {
-          if (response) {
-            self.$store.commit('userDataChange', response.data.data)
-            localStorage.setItem('member_type', response.data.data.member_type)
-            if (response.data.data.member_type === '091') {
-              if (response.data.data.reg_present === '11') {
-                self.$store.commit('setShowRegisterTicket', false)
-              } else {
-                self.$store.commit('setShowRegisterTicket', true)
-              }
-            } else {
-              self.$store.commit('setShowRegisterTicket', false)
-            }
-          }
+          self.$store.commit('userDataChange', response.data.data)
+          localStorage.setItem('member_type', response.data.data.member_type)
         })
       }
     }
