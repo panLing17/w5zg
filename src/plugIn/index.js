ddd64f60d65bfd97c5f43c594c37c6726b596dc7
panling <panlingfn@126.com>
2018-05-29 16:58:00 +0800

    update 分享优化，增加首页分享

--- src/plugIn/index.js
+++ src/plugIn/index.js
@@ -2,7 +2,6 @@ import confirm from './confirm/index.js'
 import loading from './loading/index.js'
 import alert from './alert/index.js'
 import shareSuccess from './shareSuccess/index.js'
-import share from './share/index'
 
 
-export default {confirm, loading, alert, shareSuccess, share }
+export default {confirm, loading, alert, shareSuccess }
