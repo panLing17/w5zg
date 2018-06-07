d4aecfdf3f6260ddfeb6dfeafed48f215f783c1d
panling <panlingfn@126.com>
2018-06-07 16:28:57 +0800

    update 分享debug关闭

--- src/plugIn/share/index.js
+++ src/plugIn/share/index.js
@@ -18,7 +18,7 @@ function wxConfig(vm, options) {
     headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
   }).then(function (response) {
     wx.config({
-      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: response.data.data.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
       timestamp: response.data.data.timestamp, // 必填，生成签名的时间戳
       nonceStr: response.data.data.nonceStr, // 必填，生成签名的随机串
