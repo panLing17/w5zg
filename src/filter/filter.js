57eebc860cca64a4aab2330390154468c866295b
panling <panlingfn@126.com>
2018-07-19 19:12:11 +0800

    update 首页8个分类4个活动gif动起来

--- src/filter/filter.js
+++ src/filter/filter.js
@@ -5,6 +5,11 @@ Vue.filter('img-filter', function (value) {
   // var url = 'http://192.168.1.119/' + value
   return url
 })
+Vue.filter('gif-filter', function (value) {
+  let url = process.env.IMG_URL + value
+  // var url = 'http://192.168.1.119/' + value
+  return url
+})
 Vue.filter('price-filter', function (value) {
   let fixNum = new Number(parseFloat(value) + 1).toFixed(2);//四舍五入之前加1
   let fixedNum = new Number(fixNum - 1).toFixed(2);//四舍五入之后减1，再四舍五入一下
