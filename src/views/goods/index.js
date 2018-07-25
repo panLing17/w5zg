88031dfded35395974d48fc36aabbe08349b7cf2
panling <panlingfn@126.com>
2018-07-25 14:41:48 +0800

    update

--- src/views/goods/index.js
+++ src/views/goods/index.js
@@ -1,8 +1,8 @@
 import goodsDetailed from './src/goodsDetailed'
 import confirmOrder from './src/confirmOrder'
-// import payment from './src/payment'
+import payment from './src/payment'
 import service from './src/customerService'
 import paymentResults from './src/paymentResults'
 export default {
-  goodsDetailed, confirmOrder, service, paymentResults
+  goodsDetailed, confirmOrder, payment, service, paymentResults
 }
