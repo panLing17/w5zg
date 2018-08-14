ef16f01ebd0311193b1fcdd0d94e7c91e03f8b16
panling <panlingfn@126.com>
2018-08-14 16:52:31 +0800

    update 现金券说明标题更改

--- src/views/my/src/myAccount/src/accountCardC/accountCardCExplain.vue
+++ src/views/my/src/myAccount/src/accountCardC/accountCardCExplain.vue
@@ -3,7 +3,7 @@
     nav-bar(background="#f70057")
       .topLeft(slot="left", @click="$router.go(-1)")
         img(src="./back.png", style="width:.586rem")
-      .topCenter(slot="center", style="color:#fff") 通用券说明
+      .topCenter(slot="center", style="color:#fff") 现金券说明
       .topRight(slot="right")
     .explainWapper
       explain(:data="datas")
