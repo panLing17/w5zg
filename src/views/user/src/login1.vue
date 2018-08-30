d850bc2b14cea58f83443f2c62b2a457f78bf0fb
panLing17 <panlingfn@126.com>
2018-08-30 13:19:55 +0800

    update 更改登录页面样式，解决安卓分享活动后的地址不正确问题

--- src/views/user/src/login1.vue
+++ src/views/user/src/login1.vue
@@ -1,35 +1,23 @@
 <template lang="pug">
   .loginBox
-    <!--nav-bar(background="white")-->
-      <!--.topLeft(slot="left")-->
-      <!--.topCenter(slot="center") 登录-->
-      <!--.topRight(slot="right")-->
-    <!--.form-->
-      <!--w-input(label-width="0", placeholder="请输入手机号", v-model="form.mobile")-->
-      <!--w-input(label-width="0", placeholder="请输入密码", input-button=true, :type="passwordType", v-model="form.pwd")-->
-        <!--div(slot="button").passwordSwitch-->
-          <!--transition(name="fade", mode="in-out")-->
-            <!--img(src="../../../assets/img/visible@2x.png", v-if="showPassword", @click="passwordSwitch(false)", style="position:absolute;right:0")-->
-          <!--transition(name="fade", mode="in-out", style="position:absolute;right:0")-->
-            <!--img(src="../../../assets/img/invisible@2x.png", v-if="!showPassword", @click="passwordSwitch(true)", style="position:absolute;right:0")-->
-      <!--button.loginButton(@click="login") 登录-->
-      <!--.bottomButton-->
-        <!--ul.forgetPassword-->
-          <!--li(@click="$router.push('/forget1')") 忘记密码-->
-          <!--li |-->
-          <!--li(@click="$router.push('/register1')") 注册-->
     .content
       .form
         .phone
           .iconWrapper
-            img.icon(:src="!iconFlag.phone?require('../../../assets/img/用户图标@2x.png'):require('../../../assets/img/用户图标-点亮@2x.png')")
-          input.input(type="number", placeholder="请输入您的手机号", v-model="form.mobile", @input="lightUp")
+            img.icon(:src="!iconFlag.phone?require('../../../assets/img/login/Icon_phone@2x.png'):require('../../../assets/img/login/Icon_phone_checked@2x.png')")
+          input.input(type="number", placeholder="手机号", v-model="form.mobile", @input="lightUp")
+          .iconWrapper(v-show="form.mobile.length", @click="clear(1)")
+            img.icon(src="../../../assets/img/login/Icon_del@2x.png")
         .code
           .iconWrapper
-            img.icon(:src="!iconFlag.code?require('../../../assets/img/密码2@2x.png'):require('../../../assets/img/密码-点亮@2x.png')")
-          input.input(type="password", placeholder="请输入密码", v-model="form.pwd", @input="lightUp")
+            img.icon(:src="!iconFlag.code?require('../../../assets/img/login/Icon_lock@2x.png'):require('../../../assets/img/login/Icon_unlock@2x.png')")
+          input.input(:type="passwordType", placeholder="密码", v-model="form.pwd", @input="lightUp")
+          .iconWrapper(v-show="form.pwd.length", @click="clear(2)", style="margin-right: .32rem;")
+            img.icon(src="../../../assets/img/login/Icon_del@2x.png")
+          .iconWrapper(@click="showPasswordFn")
+            img.icon(:src="passwordType=='password'?require('../../../assets/img/login/Icon_eye_unlook@2x.png'):require('../../../assets/img/login/Icon_eye@2x.png')")
+        .forget(@click="$router.push('/forget1')") 忘记密码
       .btn(@click="login") 登入
-      .forget(@click="$router.push('/forget1')") 忘记密码
 </template>
 
 <script>
@@ -42,7 +30,6 @@
     computed: mapState(['userData']),
     data () {
       return {
-        showPassword: false,
         passwordType: 'password',
         form: {
           mobile: '',
@@ -58,16 +45,17 @@
     mounted () {
     },
     methods: {
-      // passwordSwitch (show) {
-      //   if (show) {
-      //     this.showPassword = true
-      //     this.passwordType = 'text'
-      //   } else {
-      //     this.showPassword = false
-      //     this.passwordType = 'password'
-      //   }
-      // },
-
+      clear(i) {
+        if (i===1) {
+          this.form.mobile = ''
+        } else {
+          this.form.pwd=''
+        }
+        this.lightUp()
+      },
+      showPasswordFn() {
+        this.passwordType = this.passwordType == 'password'? 'text':'password'
+      },
       login () {
         if (!this.phoneReg.test(this.form.mobile)) {
           this.$message.error('手机号格式不正确！')
@@ -116,44 +104,6 @@
 </script>
 
 <style scoped>
-  /*.form{*/
-    /*padding-top: 1.3rem;*/
-    /*display: flex;*/
-    /*flex-direction: column;*/
-    /*align-items: center;*/
-  /*}*/
-  /*.passwordSwitch{*/
-    /*position: relative;*/
-    /*display: flex;*/
-    /*height: .8rem;*/
-    /*align-items: center;*/
-  /*}*/
-  /*.passwordSwitch img{*/
-    /*width: .6rem;*/
-  /*}*/
-  /*.loginButton {*/
-    /*margin-top: 1rem;*/
-    /*width: 7rem;*/
-    /*height: 1rem;*/
-    /*background-color: rgb(245,0,87);*/
-    /*color: white;*/
-    /*border: none;*/
-    /*outline: none;*/
-    /*border-radius: .5rem;*/
-  /*}*/
-  /*.bottomButton {*/
-    /*margin-top: 3rem;*/
-    /*width: 100%;*/
-
-    /*left: 0;*/
-    /*display: flex;*/
-    /*justify-content: center;*/
-  /*}*/
-  /*.forgetPassword {*/
-    /*width: 2.5rem;*/
-    /*display: flex;*/
-    /*justify-content: center;*/
-  /*}*/
   input::-webkit-input-placeholder{
     color:#999;
   }
@@ -165,25 +115,25 @@
   }
   .content {
     background: #fff;
+    width: 8rem;
     height: calc(100vh - 1.3rem);
+    margin: 0 auto;
   }
   .form {
-    padding: 1.5rem 1rem 0;
+    padding-top: 1.33rem;
   }
   .phone, .code {
-    height: 1rem;
+    height: 1.17rem;
     display: flex;
     align-items: center;
-    border-bottom: 1px solid #f2f2f2;
+    border-bottom: 1px solid #999;
   }
   .iconWrapper {
     flex: none;
-    width: 1rem;
   }
-  .phone .icon {
-    width: .4rem;
-  }
+
   .input {
+    margin-left: .19rem;
     flex: 1;
     width: 100%;
     height: 100%;
@@ -193,7 +143,7 @@
     font-size: .4rem;
   }
   .code {
-    margin-top: 1.2rem;
+    margin-top: .4rem;
   }
   .codeBtn {
     flex: none;
@@ -205,12 +155,12 @@
     border-radius: .2rem;
     font-size: .4rem;
   }
-  .code .icon {
-    width: .46rem;
+  .icon {
+    width: .64rem;
   }
   .btn {
-    margin: 2.37rem auto 0;
-    width: 6rem;
+    margin: .42rem auto 0;
+    width: 6.9rem;
     height: 1rem;
     font-size: .4rem;
     color: #fff;
@@ -223,8 +173,10 @@
     pointer-events: none;
   }
   .forget {
-    text-align: center;
-    margin-top: 2rem;
-    font-size: .26rem;
+    text-align: right;
+    margin-top: .21rem;
+    font-size: .32rem;
+    line-height: .64rem;
+    color: #777;
   }
 </style>
