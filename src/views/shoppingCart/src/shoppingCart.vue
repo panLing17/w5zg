abc20467f0fcdf256bdbb500af65d3c883665f4b
panLing17 <panlingfn@126.com>
2018-08-30 14:30:02 +0800

    fix 解决首次进入购物车页面，快递配送页底部没有结算、全选按钮

--- src/views/shoppingCart/src/shoppingCart.vue
+++ src/views/shoppingCart/src/shoppingCart.vue
@@ -299,6 +299,13 @@
           params: {},
         }).then(function (response) {
           self.$store.commit('shoppingCartGoodsNumChange', response.data.data)
+          if (self.nowTab == 0 && response.data.data.sendNum > 0) {
+            self.settlementShow = true
+          } else if (self.nowTab==1 && response.data.data.carryNum > 0) {
+            self.settlementShow = true
+          } else {
+            self.settlementShow = false
+          }
         })
       },
       tabChange(num) {
@@ -312,13 +319,7 @@
           this.$router.push('/shoppingCart/self')
         }
 
-        if (num == 0 && this.shoppingCartGoodsNum.carryNum > 0) {
-          this.settlementShow = true
-        } else if (num == 1 && this.shoppingCartGoodsNum.sendNum > 0) {
-          this.settlementShow = true
-        } else {
-          this.settlementShow = false
-        }
+
         /* 请求购物车上面数量 */
         this.getGoodsNum()
         /* 关闭整理操作 */
